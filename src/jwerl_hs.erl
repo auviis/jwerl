572a0387fe7f4b109a5d93c7fe79e99f64e4743b
telcomxn0 <nhnhat@gmail.com>
2022-07-01 09:34:21 +0700

    Update jwerl_hs.erl

--- src/jwerl_hs.erl
+++ src/jwerl_hs.erl
@@ -4,7 +4,11 @@
 -export([sign/3, verify/4]).
 
 sign(ShaBits, Key, Data) ->
-  crypto:hmac(algo(ShaBits), Key, Data).
+  %crypto:hmac(algo(ShaBits), Key, Data).
+  %crypto:hmac(DigestFunc, Key, Data)
+  HMAC = crypto:mac_init(hmac, algo(ShaBits), Key),
+  HMAC1 = crypto:mac_update(HMAC, Data),
+  crypto:mac_final(HMAC1).
 
 verify(ShaBits, Key, Data, Signature) ->
   Signature == sign(ShaBits, Key, Data).
